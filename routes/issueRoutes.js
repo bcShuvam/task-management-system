c99a282be6e2281699d4b778a67d84c0d5dbe53a
Shuvam BC <bcshuvam@gmail.com>
2025-06-15 12:20:04 +0545

    SB-TMS-4: added upload issue image feature

--- routes/issueRoutes.js
+++ routes/issueRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getMyIssue, getCompanyIssues, createIssue, applyIssue, deleteIssue  } = require('../controller/issueController');
+const { getMyIssue, getCompanyIssues, applyIssue, deleteIssue  } = require('../controller/issueController');
+const issueController = require('../controller/createIssueController');
 
 router.get('/my/:id', getMyIssue);
 router.get('/company/:id', getCompanyIssues);
-router.post('/create/:id', createIssue);
+router.post(
+  '/create/:id',
+  issueController.upload.single('issueImage'),  // ✅ This is crucial
+  issueController.createIssue
+);
 router.put('/apply/:id', applyIssue);
 router.delete('/delete/:id', deleteIssue);
 
